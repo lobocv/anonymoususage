43b965f8a20ae6af4140d5b857c291191d9e7d08
clobo <clobo@sensoft.ca>
2015-09-03 14:07:37 -0400

    draw plots in 1 column, added figure legend

--- anonymoususage/analysis.py
+++ anonymoususage/analysis.py
@@ -8,36 +8,36 @@ from matplotlib.dates import DateFormatter
 from tools import *
 
 
-def plot_stat(dbconn, table_names, uuid=None, date_limits=(None, None), datefmt=None):
-
-    max_cols = 3
-    nrows = max(1, len(table_names) / max_cols)
-    ncols = len(table_names) % max_cols
-    fig, plots = plt.subplots(nrows=nrows, ncols=ncols)
-    if len(table_names) == 1:
+def _get_figure(n_items, max_cols):
+    ncols = 1# + n_items / max_cols
+    nrows = n_items #min(n_items, max_cols)
+    figure = plt.Figure()
+    figure, plots = plt.subplots(nrows=nrows, ncols=ncols, sharex=True)
+    if n_items == 1:
         plots = [plots]
-    if uuid:
-        plt.title(uuid)
+    else:
+        plots = plots.flatten()
+    return figure, plots[:n_items]
+
+
+def plot_statistic(dbconn, table_names, uuid=None, date_limits=(None, None), datefmt=None):
+    fig, plots = _get_figure(len(table_names), max_cols=3)
 
     plotted_tables = set()
-    if uuid is None:
-        uuids = get_uuid_list(dbconn)
-    else:
-        uuids = [uuid]
+    uuids = get_uuid_list(dbconn) if uuid is None else [uuid]
     for uuid in uuids:
         for ii, table_name in enumerate(table_names):
-
-            plot = plots[ii]
-            if datefmt:
-                plot.xaxis.set_major_formatter(DateFormatter(datefmt))
-            plot.set_xlabel('Date')
-            plot.set_ylabel('Count')
-
             data = get_datetime_sorted_rows(dbconn, table_name, uuid=uuid, column='Count')
-            if data:
+            if len(data) > 1:
+                plot = plots.flatten()[ii]
+                plot.set_title(table_name)
                 times, counts = zip(*data)
                 plot.plot_date(times, counts, '-o', label=table_name)
                 plotted_tables.add(table_name)
+                if datefmt:
+                    plot.xaxis.set_major_formatter(DateFormatter(datefmt))
+                plot.set_xlabel('Date')
+                plot.set_ylabel('Count')
             else:
                 logging.warning('No data for %s. Omitting from plot.' % table_name)
 
@@ -45,12 +45,8 @@ def plot_stat(dbconn, table_names, uuid=None, date_limits=(None, None), datefmt=
             logging.warning('No data for found. Failed to create plot.')
             return
 
-    legend_fields = plt.legend(plotted_tables, loc='center left', bbox_to_anchor=(0, 1),
-                               fancybox=True, ncol=max(1, 3 * (len(plotted_tables) / 3)))
-
-    legend_uuids = plt.legend(uuids, loc='center right', bbox_to_anchor=(1, 1), title='UUID',
-                              fancybox=True, ncol=max(1, 3 * (len(plotted_tables) / 3)))
-    plt.gca().add_artist(legend_fields)
+    handles, labels = plots[0].get_legend_handles_labels()
+    plt.figlegend(handles, plotted_tables, loc=' left', ncol=max(1, 3 * (len(plotted_tables) / 3)), labelspacing=0.)
 
     if date_limits[0] and date_limits[1]:
         plots.set_xlim(*date_limits)
