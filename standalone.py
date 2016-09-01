361eeb8f90ff177a94ddedd49a5813aa9dcabb4c
clobo <clobo@sensoft.ca>
2016-09-01 14:08:27 -0400

    created a standalone.py file that launches a usage tracker and opens communication on a port

---
+++ standalone.py
@@ -0,0 +1,40 @@
+from anonymoususage import AnonymousUsageTracker
+import sys
+
+__help__ = \
+    '''
+    This application creates and starts a new anonymous usage tracker. Use the API to communicate with
+    the tracker through a socket to setup and track statistics in your application.
+
+
+    Call Arguments:
+         UUID        - Unique identifier for the user being tracked (string)
+         HOST        - Host address to open the socket under (use 'localhost' or '127.0.0.1' for local)
+         PORT        - Port to open the socket under
+         FILEPATH    - File path to write the database
+
+    Example
+
+    standalone 'USER100049' localhost 1213 ./my_statistics.db
+
+    '''
+
+try:
+    UUID, HOST, PORT, FILEPATH = sys.argv[1:]
+except Exception as e:
+    print __help__
+    sys.exit(-1)
+
+print 'Creating Usage Tracker..'
+usage_tracker = AnonymousUsageTracker(uuid=UUID,
+                                      filepath=FILEPATH)
+
+print 'Opening connection..'
+if HOST in ('localhost', '127.0.0.1'):
+    HOST = ''
+s = usage_tracker.open_socket(HOST, int(PORT))
+
+print 'Starting tracker..'
+usage_tracker.monitor_socket(s)
+print 'Stopping tracker..'
+
--- test_ipc.py
+++ test_ipc.py
@@ -1,89 +1,72 @@
 __author__ = 'calvin'
 
-import sys
 import time
 import json
-import datetime
-from anonymoususage import AnonymousUsageTracker
+import socket
 
-
-mode = sys.argv[1]
 
 HOST = ''
-PORT = 1215
-if mode == '0':
+PORT = 1213
 
-    usage_tracker = AnonymousUsageTracker(uuid="ASDFGH",
-                                          filepath='./test.db')
-    usage_tracker.track_statistic('lines')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
-    s = usage_tracker.open_socket(HOST, PORT)
-    usage_tracker.monitor_socket(s)
+cmds = [{'command': 'ACT', 'trackable': '', 'action': 'track_statistic', 'args': ('grids',)},
+        {'command': 'ACT', 'trackable': '', 'action': 'track_statistic', 'args': ('lines',)},
+        {'command': 'ACT', 'trackable': '', 'action': 'track_time', 'args': ('run_time',)},
+        {'command': 'ACT', 'trackable': '', 'action': 'track_sequence', 'args': ('my_sequence', ['A', 'B', 'C', 'D'])},
+        {'command': 'ACT', 'trackable': '', 'action': 'track_state', 'args': ('units', 'Metric')},
+        {'command': 'ACT', 'trackable': '', 'action': 'get_table_info', 'args': ()},
+        ]
 
-
-elif mode == '1':
-
-    import socket
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-
-    cmds = [{'command': 'ACT', 'trackable': '', 'action': 'track_statistic', 'args': ('grids',)},
-            {'command': 'ACT', 'trackable': '', 'action': 'track_statistic', 'args': ('lines',)},
-            {'command': 'ACT', 'trackable': '', 'action': 'track_time', 'args': ('run_time',)},
-            {'command': 'ACT', 'trackable': '', 'action': 'track_sequence', 'args': ('my_sequence', ['A', 'B', 'C', 'D'])},
-            {'command': 'ACT', 'trackable': '', 'action': 'track_state', 'args': ('units', 'Metric')},
-            {'command': 'ACT', 'trackable': '', 'action': 'get_table_info', 'args': ()},
-            ]
-
-    for cmd in cmds:
-        if not isinstance(cmd, dict):
-            cmd()
-        else:
-            s.send(json.dumps(cmd))
-            print s.recv(1024)
+for cmd in cmds:
+    if not isinstance(cmd, dict):
+        cmd()
+    else:
+        s.send(json.dumps(cmd))
+        print s.recv(1024)
 
 
-    cmds = [# Statistic
-            {'command': 'GET', 'trackable': 'grids', 'field': 'count'},
-            {'command': 'SET', 'trackable': 'grids', 'field': 'count', 'value': 50},
-            {'command': 'GET', 'trackable': 'grids', 'field': 'count'},
+cmds = [# Statistic
+        {'command': 'GET', 'trackable': 'grids', 'field': 'count'},
+        {'command': 'SET', 'trackable': 'grids', 'field': 'count', 'value': 50},
+        {'command': 'GET', 'trackable': 'grids', 'field': 'count'},
 
-        # State
-            {'command': 'GET', 'trackable': 'units', 'field': 'state'},
-            {'command': 'SET', 'trackable': 'units', 'field': 'state', 'value': 'Metric'},
-            {'command': 'GET', 'trackable': 'units', 'field': 'state'},
+    # State
+        {'command': 'GET', 'trackable': 'units', 'field': 'state'},
+        {'command': 'SET', 'trackable': 'units', 'field': 'state', 'value': 'Metric'},
+        {'command': 'GET', 'trackable': 'units', 'field': 'state'},
 
-            # Timer
-            {'command': 'GET', 'trackable': 'run_time', 'field': 'total_seconds'},
-            {'command': 'ACT', 'trackable': 'run_time', 'action': 'start_timer', 'args': ()},
-            lambda: time.sleep(1),
-            {'command': 'ACT', 'trackable': 'run_time', 'action': 'pause_timer', 'args': ()},
-            lambda: time.sleep(5),
-            {'command': 'ACT', 'trackable': 'run_time', 'action': 'resume_timer', 'args': ()},
-            lambda: time.sleep(1),
-            {'command': 'ACT', 'trackable': 'run_time', 'action': 'stop_timer', 'args': ()},
-            {'command': 'GET', 'trackable': 'run_time', 'field': 'total_seconds'},
-            {'command': 'GET', 'trackable': 'run_time', 'field': 'total_minutes'},
-            {'command': 'GET', 'trackable': 'run_time', 'field': 'total_hours'},
-            {'command': 'GET', 'trackable': 'run_time', 'field': 'total_days'},
+        # Timer
+        {'command': 'GET', 'trackable': 'run_time', 'field': 'total_seconds'},
+        {'command': 'ACT', 'trackable': 'run_time', 'action': 'start_timer', 'args': ()},
+        lambda: time.sleep(1),
+        {'command': 'ACT', 'trackable': 'run_time', 'action': 'pause_timer', 'args': ()},
+        lambda: time.sleep(5),
+        {'command': 'ACT', 'trackable': 'run_time', 'action': 'resume_timer', 'args': ()},
+        lambda: time.sleep(1),
+        {'command': 'ACT', 'trackable': 'run_time', 'action': 'stop_timer', 'args': ()},
+        {'command': 'GET', 'trackable': 'run_time', 'field': 'total_seconds'},
+        {'command': 'GET', 'trackable': 'run_time', 'field': 'total_minutes'},
+        {'command': 'GET', 'trackable': 'run_time', 'field': 'total_hours'},
+        {'command': 'GET', 'trackable': 'run_time', 'field': 'total_days'},
 
-            # Sequence
-            {'command': 'GET', 'trackable': 'my_sequence', 'field': 'count'},
-            {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'A'},
-            {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'B'},
-            {'command': 'GET', 'trackable': 'my_sequence', 'field': 'sequence'},
-            {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'C'},
-            {'command': 'GET', 'trackable': 'my_sequence', 'field': 'checkpoint'},
-            {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'D'},
-            {'command': 'GET', 'trackable': 'my_sequence', 'field': 'count'},
+        # Sequence
+        {'command': 'GET', 'trackable': 'my_sequence', 'field': 'count'},
+        {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'A'},
+        {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'B'},
+        {'command': 'GET', 'trackable': 'my_sequence', 'field': 'sequence'},
+        {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'C'},
+        {'command': 'GET', 'trackable': 'my_sequence', 'field': 'checkpoint'},
+        {'command': 'SET', 'trackable': 'my_sequence', 'field': 'checkpoint', 'value': 'D'},
+        {'command': 'GET', 'trackable': 'my_sequence', 'field': 'count'},
 
-    ]
+]
 
-    for cmd in cmds:
-        if not isinstance(cmd, dict):
-            cmd()
-        else:
-            s.send(json.dumps(cmd))
-            print s.recv(1024)
+for cmd in cmds:
+    if not isinstance(cmd, dict):
+        cmd()
+    else:
+        s.send(json.dumps(cmd))
+        print s.recv(1024)
 
