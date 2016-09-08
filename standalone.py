02839549cbfecc74b6b76e88bf20fac8569cdf0c
clobo <clobo@sensoft.ca>
2016-09-08 16:09:43 -0400

    added improve CLI / arg parsing

--- standalone.py
+++ standalone.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import sys
+import argparse
 
 from anonymoususage import AnonymousUsageTracker
 from anonymoususage.tables import TRACKABLES
@@ -36,28 +37,43 @@ __help__ = \
                TRACKABLES='\n'.join(t.api_help() for t in TRACKABLES))
 
 
+def run_server(uuid, host, port, db_path, config=None):
 
-try:
-    UUID, HOST, PORT, FILEPATH = sys.argv[1:]
-except Exception as e:
-    print __help__
-    sys.exit(-1)
+    logging.basicConfig(filename=os.path.splitext(FILEPATH)[0] + '.log', level=logging.DEBUG)
+
+    logging.info('Creating Usage Tracker..')
+    if config:
+        usage_tracker = AnonymousUsageTracker.load_from_configuration(config, uuid, filepath=db_path)
+    else:
+        usage_tracker = AnonymousUsageTracker(uuid=uuid,
+                                              filepath=db_path)
+
+    logging.info('Opening connection..')
+    if host in ('localhost', '127.0.0.1'):
+        host = ''
+
+    sock = usage_tracker.open_socket(host, port)
+
+    logging.info('Starting tracker..')
+    usage_tracker.monitor_socket(sock)
+
+    logging.info('Stopping tracker..')
 
 
-logging.basicConfig(filename=os.path.splitext(FILEPATH)[0] + '.log', level=logging.DEBUG)
+if __name__ == '__main__':
 
-logging.info('Creating Usage Tracker..')
-usage_tracker = AnonymousUsageTracker(uuid=UUID,
-                                      filepath=FILEPATH)
+    parser = argparse.ArgumentParser('Start a server that can be communicated through sockets to track user behaviour')
+    parser.add_argument('uuid', nargs=1, type=str)
+    parser.add_argument('host', nargs=1, type=str)
+    parser.add_argument('port', nargs=1, type=int)
+    parser.add_argument('db_path', nargs=1, type=str)
+    parser.add_argument('--config', nargs=1, default=[None], type=str, dest='config')
 
-logging.info('Opening connection..')
-if HOST in ('localhost', '127.0.0.1'):
-    HOST = ''
-
-sock = usage_tracker.open_socket(HOST, int(PORT))
-
-logging.info('Starting tracker..')
-usage_tracker.monitor_socket(sock)
-
-logging.info('Stopping tracker..')
-
+    args = parser.parse_args()
+    try:
+        UUID, HOST, PORT, FILEPATH = sys.argv[1:]
+    except Exception as e:
+        print __help__
+        sys.exit(-1)
+    else:
+        run_server(args.uuid[0], args.host[0], args.port[0], args.db_path[0], config=args.config[0])
