15fd199ebfe96448c5546e176947ebd364965470
clobo <clobo@sensoft.ca>
2016-11-10 08:32:21 -0500

    changed sequence attribute into @property that returns a tuple representation of the underlying _sequenc deque attribute (allows the return value to be json serializable)

--- anonymoususage/tables/sequence.py
+++ anonymoususage/tables/sequence.py
@@ -32,13 +32,13 @@ class Sequence(Table):
     def __init__(self, name, tracker, checkpoints, *args, **kwargs):
         super(Sequence, self).__init__(name, tracker, *args, **kwargs)
         self._checkpoints = checkpoints
-        self.sequence = deque([], maxlen=len(checkpoints))
+        self._sequence = deque([], maxlen=len(checkpoints))
 
     def insert(self, checkpoint):
         if checkpoint in self._checkpoints:
-            self.sequence.append(checkpoint)
+            self._sequence.append(checkpoint)
             logging.debug('{cp} added to sequence "{s.name}"'.format(cp=checkpoint, s=self))
-            if len(self.sequence) == len(self._checkpoints) and all(imap(eq, self.sequence, self._checkpoints)):
+            if len(self._sequence) == len(self._checkpoints) and all(imap(eq, self._sequence, self._checkpoints)):
                 # Sequence is complete. Increment the database
                 dt = datetime.datetime.now().strftime(self.time_fmt)
                 count = self.count + 1
@@ -51,6 +51,7 @@ class Sequence(Table):
                     logger.error(e)
                 else:
                     self.count = count
+                    self._sequence.clear()
                     logging.debug("Sequence {s.name} complete, count set to {s.count}".format(s=self))
         else:
             raise InvalidCheckpointError(checkpoint)
@@ -58,13 +59,17 @@ class Sequence(Table):
     @property
     def checkpoint(self):
         try:
-            return self.sequence[-1]
+            return self._sequence[-1]
         except IndexError:
             return None
 
     @checkpoint.setter
     def checkpoint(self, checkpoint):
         self.insert(checkpoint)
+
+    @property
+    def sequence(self):
+        return tuple(self._sequence)
 
     @property
     def checkpoints(self):
@@ -77,14 +82,14 @@ class Sequence(Table):
         """
         Remove the last check point.
         """
-        if len(self.sequence):
-            return self.sequence.pop()
+        if len(self._sequence):
+            return self._sequence.pop()
 
     def clear_checkpoints(self):
         """
         Clear all completed check points.
         """
-        self.sequence.clear()
+        self._sequence.clear()
 
     def advance_to_checkpoint(self, checkpoint):
         """
@@ -94,7 +99,7 @@ class Sequence(Table):
             for cp in self._checkpoints:
                 self.insert(cp)
                 if cp == checkpoint:
-                    break
+                    return cp
         else:
             raise InvalidCheckpointError(checkpoint)
 
